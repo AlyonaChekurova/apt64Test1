3cba9579de7360af69b41b71ac5d61fdf93292ef
Alyona Chekurova <chekurova97@gmail.com>
2021-01-22 10:08:38 +0300

    Added authorization, search, add to cart demonstration tests and always failed test. Created system variables to get parameters from Jenkins

--- src/test/java/pages/StartPage.java
+++ src/test/java/pages/StartPage.java
@@ -1,6 +1,14 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
+import helpers.Waiters;
+import io.qameta.allure.Step;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 /**
  * Start page object.
@@ -15,4 +23,194 @@ public class StartPage {
      * Maximum time to wait per seconds.
      */
     private final static int MAX_WAIT = 10;
+
+    /**
+     * Search input field.
+     */
+    private final static String SEARCH_INPUT_LOCATOR = ".input-search";
+
+    /**
+     * Authorization form window.
+     */
+    private final static String LOGIN_BOX_LOCATOR = ".login-box.logout-box";
+
+    /**
+     * Login field.
+     */
+    private final static String LOGIN_FIELD_LOCATOR = "input[name = 'login']";
+    /**
+     * Password field.
+     */
+    private final static String PASSWORD_FIELD_LOCATOR = "input[name = 'password']";
+
+    /**
+     * Preloader locator to check its invisibility.
+     */
+    private final static String PRELOADER_LOCATOR = ".preloader";
+
+    /**
+     * Login error message text locator.
+     */
+    private final static String LOGIN_ERROR_MESSAGE_LOCATOR = ".login-error";
+
+    /**
+     * Cart counter locator.
+     */
+    private final static String CART_COUNTER_LOCATOR = "#itogCount";
+
+    /**
+     * Button shows authorization form.
+     */
+    @FindBy(css = ".authorization.login-button")
+    private WebElement authButton;
+
+    /**
+     * Login button.
+     */
+    @FindBy(css = "#signIn2")
+    private WebElement loginButton;
+
+    /**
+     * Search results text.
+     */
+    @FindBy(css = ".search-word")
+    private WebElement searchResult;
+
+    /**
+     * Add to cart button in the first search result.
+     */
+    @FindBy(css =
+            ".search-block-name>.search-block>ul>li:nth-child(1)>div>.add-to-cart.buy")
+    private WebElement addToCartButton;
+
+    /**
+     * Class constructor.
+     * Initializes WebDriver instance.
+     *
+     * @param webDriver - WebDriver instance
+     */
+    public StartPage(WebDriver webDriver) {
+        this.driver = webDriver;
+
+        PageFactory.initElements(webDriver, this);
+    }
+
+    /**
+     * Click button to show authorization form method.
+     *
+     * @return StartPage object
+     */
+    @Step("Нажать кнопку показа формы авторизации.")
+    public final StartPage clickAuthButton() {
+        WebDriverWait wait = new WebDriverWait(driver, MAX_WAIT);
+
+        Waiters.waitClickability(driver, MAX_WAIT, authButton);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(PRELOADER_LOCATOR)));
+        authButton.click();
+        ((JavascriptExecutor) driver).executeScript("window.stop();");
+        return this;
+    }
+
+    /**
+     * Try to authorize method.
+     *
+     * @param login    - login for authorization
+     * @param password - password for authorization
+     * @return error message text
+     */
+    @Step("Авторизоваться с логином '{login}' и паролем '{password}'")
+    public final String checkAuth(final String login,
+                                  final String password) {
+        clickAuthButton();
+        ((JavascriptExecutor) driver).executeScript("window.stop();");
+        Waiters.waitVisibility(driver, MAX_WAIT,
+                driver.findElement(By.cssSelector(LOGIN_BOX_LOCATOR)));
+
+        driver.findElement(By.cssSelector(LOGIN_FIELD_LOCATOR)).sendKeys(login);
+        ((JavascriptExecutor) driver).executeScript("window.stop();");
+        driver.findElement(By.cssSelector(PASSWORD_FIELD_LOCATOR)).sendKeys(password);
+        ((JavascriptExecutor) driver).executeScript("window.stop();");
+
+        WebDriverWait wait = new WebDriverWait(driver, MAX_WAIT);
+        WebElement preloader = driver.findElement(By.cssSelector(PRELOADER_LOCATOR));
+        if (preloader.isDisplayed())
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(
+                    By.cssSelector(PRELOADER_LOCATOR)));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.cssSelector
+                (LOGIN_ERROR_MESSAGE_LOCATOR));
+        Waiters.waitVisibility(driver, MAX_WAIT, driver
+                .findElement(By.cssSelector(LOGIN_ERROR_MESSAGE_LOCATOR)));
+
+        return errorMessage.getText();
+    }
+
+    /**
+     * Get search results method.
+     *
+     * @param searchQuery - query to search for
+     * @return StartPage object
+     */
+    @Step("Выполнить поиск по запросу {searchQuery}")
+    public final StartPage getSearchResults(final String searchQuery) {
+        driver.findElement(By.cssSelector(SEARCH_INPUT_LOCATOR)).clear();
+        driver.findElement(By.cssSelector(SEARCH_INPUT_LOCATOR))
+                .sendKeys(searchQuery + Keys.ENTER);
+        Waiters.waitVisibility(driver, MAX_WAIT, searchResult);
+
+        return this;
+    }
+
+    /**
+     * Get search result text method.
+     *
+     * @param searchQuery - query to search for
+     * @return search result text
+     */
+    @Step("Получить текст о результатах поиска по запросу {searchQuery}")
+    public final String getSearchResultText(final String searchQuery) {
+        getSearchResults(searchQuery);
+
+        return searchResult.getText();
+    }
+
+    /**
+     * Click add to cart button method.
+     *
+     * @param searchQuery - query to search for
+     * @return StartPage object when count is changed
+     */
+    @Step("Выполнить поиск по запросу {searchQuery} и добавить в корзину товар, "
+            + "первый по результатам поиска")
+    public final StartPage clickAddToCartButton(final String searchQuery) {
+        WebDriverWait wait = new WebDriverWait(driver, MAX_WAIT);
+        getSearchResults(searchQuery);
+        ((JavascriptExecutor) driver).executeScript("window.stop();");
+        final String currentText = driver.findElement(
+                By.cssSelector(CART_COUNTER_LOCATOR)).getText();
+        Waiters.waitClickability(driver, MAX_WAIT, addToCartButton);
+        addToCartButton.click();
+
+        wait.until((WebDriver driver) -> !driver.findElement(
+                By.cssSelector(CART_COUNTER_LOCATOR))
+                .getText().equals(currentText));
+
+        return this;
+    }
+
+    /**
+     * Get cart count value method.
+     *
+     * @return cart count value
+     */
+    @Step("Получить значение счетчика товаров в корзине")
+    public final int getCartCount() {
+        WebDriverWait wait = new WebDriverWait(driver, MAX_WAIT);
+        wait.withTimeout(Duration.ofSeconds(MAX_WAIT));
+        WebElement cartCount = driver.findElement(
+                By.cssSelector(CART_COUNTER_LOCATOR));
+
+        return Integer.parseInt(cartCount.getText());
+    }
 }
--- src/test/java/tests/StartPageTests.java
+++ src/test/java/tests/StartPageTests.java
@@ -3,8 +3,9 @@ package tests;
 import helpers.DriverFactory;
 import helpers.ParametersProvider;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.StartPage;
 
 import java.io.IOException;
 
@@ -21,7 +22,22 @@ public class StartPageTests {
     /**
      * Name of browser where tests should run.
      */
-    private String browserName = "chrome";
+    private String browserName = System.getProperty("browserName");
+
+    /**
+     * Login to authorization test.
+     */
+    private String login = System.getProperty("login");
+
+    /**
+     * Password to authorization test.
+     */
+    private String password = System.getProperty("password");
+
+    /**
+     * Query text to search test.
+     */
+    private String query = System.getProperty("searchQuery");
 
     /**
      * Suite setup.
@@ -35,6 +51,59 @@ public class StartPageTests {
         driver.get(webUrl);
     }
 
+    /**
+     * Wrong data authorization test.
+     *
+     * @throws IOException when config file is unavailable
+     */
+    @Test(description = "Тест авторизации с некорректными логином и паролем")
+    public final void wrongLoginPasswordTest() throws IOException {
+        StartPage startPage = new StartPage(driver);
+
+        String message = startPage.checkAuth(login, password);
+
+        Assert.assertEquals(message,
+                ParametersProvider.getProperty("loginErrorMessage"));
+    }
+
+    /**
+     * Search field working test: successful search.
+     */
+    @Test(description = "Тест работы строки поиска: успешный поиск")
+    public final void successfulSearchTest() {
+        StartPage startPage = new StartPage(driver);
+
+        String res = startPage.getSearchResultText(query);
+
+        Assert.assertTrue(res.endsWith("НАЙДЕНО:"));
+    }
+
+    /**
+     * Add to cart displaying test.
+     */
+    @Test(description = "Тест отображения добавления в корзину")
+    public final void addToCartTest() {
+        StartPage startPage = new StartPage(driver);
+
+        int initCount = startPage.getCartCount();
+
+
+        int finalCount = startPage.clickAddToCartButton(query)
+                .getCartCount();
+
+        Assert.assertEquals(finalCount, initCount + 1);
+
+    }
+
+    /**
+     * Get page title test.
+     */
+    @Test(description = "Тест заголовка страницы")
+    public final void checkPageTitle() {
+        String pageTitle = driver.getTitle();
+
+        Assert.assertNotEquals(driver.getTitle(), pageTitle);
+    }
 
     /**
      * Suite teardown.
